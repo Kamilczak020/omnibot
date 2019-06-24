61a5291a06a5e9296524f9cddfe0aee61f51d2fd
Kamilczak020 <kamilczak020@gmail.com>
2019-06-24 17:14:13 +0200

    Renamed commands to listcommands

--- src/handler/helpHandler.js
+++ src/handler/helpHandler.js
@@ -27,7 +27,7 @@ export class HelpHandler extends BaseHandler {
         break;
       }
 
-      case 'commands': {
+      case 'listcommands': {
         const queryer = await this.client.fetchUser(message.dataValues.author);
         const dmChannel = queryer.dmChannel ? queryer.dmChannel : await queryer.createDM();
         const embed = new RichEmbed({
