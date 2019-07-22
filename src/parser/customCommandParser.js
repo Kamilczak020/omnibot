79702ac9799f54f170bf8a0d1217c083ea3c9e9e
Kamilczak020 <kamilczak020@gmail.com>
2019-07-22 22:52:22 +0200

    Implemented custom command parser

--- src/parser/customCommandParser.js
+++ src/parser/customCommandParser.js
@@ -2,9 +2,19 @@
 import { BaseParser } from './baseParser';
 import { mustExist } from '../util';
 import { Command } from '../model/command';
+import { CustomCommand } from '../model/customCommand';
 import { CommandData } from '../model/commandData';
+import { isNil } from 'lodash';
 
-export class EchoParser extends BaseParser {
+export class CustomCommandParser extends BaseParser {
+  async check(msg) {
+    const cmd = mustExist(msg.dataValues.body.split(' ')[0]);
+    this.logger.debug('command parsed:', cmd);
+
+    const matchedCommand = await CustomCommand.findOne({ where: { name: this.stripPrefix(cmd) }});
+    return cmd.startsWith(this.options.prefix) && !isNil(matchedCommand);
+  }
+
   async parse(msg) {
     const cmd = mustExist(this.stripPrefix(msg.dataValues.body.split(' ')[0]));
     const body = mustExist(msg.dataValues.body.split(' ').slice(1).join(' '));
